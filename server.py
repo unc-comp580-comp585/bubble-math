9f0fce673175e0eefa3f6d0b0ae508900df16fa8
Deven Bowers <drdesai@cs.unc.edu>
2017-02-06 00:14:45 +0000

    add vibration server

--- server.py
+++ server.py
@@ -1,19 +1,19 @@
 from sanic import Sanic
 from sanic.response import json
-from controller import Controller
+from controller import Controller as XboxCtrl
 
-controller = None
 app = Sanic("Vibration Server")
 
 @app.route("/vibrate")
 async def vibrate(request):
     jsonify = request.args
-    intensity = float(args['intensity']) if 'intensity' in args else float(1.0)
-    duration = int(args['duration']) if 'duration' in args else 100
-    Controller.rumble(intensity, duration)
+    print(jsonify)
+    intensity = float(jsonify['intensity'][0]) if 'intensity' in jsonify else float(1.0)
+    duration = int(jsonify['duration'][0]) if 'duration' in jsonify else 100
+    ctrl.rumble(intensity, duration)
     return json({"res": True})
 
 
 if __name__ == '__main__':
-    global controller
-    controller = Controller(dead_zone=16000) 
+    ctrl = XboxCtrl(dead_zone=16000) 
+    app.run(host="0.0.0.0", port="8000", debug=True)
