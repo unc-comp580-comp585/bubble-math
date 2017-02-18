dd167280b4ad35721c2e6d5966512410a87de5bc
Federico Menozzi <federicogmenozzi@gmail.com>
2017-02-18 12:17:43 -0500

    This was bugging me

--- Graphics.js
+++ Graphics.js
@@ -2,5 +2,5 @@ var Graphics = {};
 
 // For now, fill in background with single color
 Graphics.drawBackground = function(game) {
-    game.stage.backgroundColor = 'rgb(50, 50, 200)';
+    game.stage.backgroundColor = '#7bafd4';
 };
--- game.js
+++ game.js
@@ -6,44 +6,48 @@ window.onload = function() {
         update: update,
     });
 
-    //Global Defines
+    // Global Defines
     var gamepad;
     var gamepad_enabled;
     var leftjs_dead_zone = 0.1;
+
     // Difficulty of Game [0-2]
     var difficulty = 0;
+
     // Grade in School [1-4]
     var grade = 1;
 
-    //BS1 Defines
+    // BS1 Defines
     var cursor;
     var wheel_map;
+
     // question -> answer
     var equation_map;
-    // questions that have been answered
+
+    // Questions that have been answered
     var answered_questions;
-    // index of current question
+
+    // Index of current question
     var question_index;
-    //var questions
+
     var questions;
     var answers;
     
-    //keyboard fallbacks
+    // Keyboard fallbacks
     var Q;
     var E;
     var Space;
     var R;
 
-    function preload()
-    {
+    function preload() {
         wheel_map = {};
         equation_map = {};
         answered_questions = {};
         question_index = 0;
-        for(var coeff = 0; coeff < 3; coeff ++){
+        for (var coeff = 0; coeff < 3; coeff ++) {
             wheel_map[''+coeff] = [];
             var addition = Math.PI / ((2 * (coeff + 1)));
-            for(var i = 0; i < 2 * Math.PI; i += addition) {
+            for (var i = 0; i < 2 * Math.PI; i += addition) {
                 var convert = i * (180 / Math.PI);
                 wheel_map[''+coeff].push(convert);
             }
@@ -51,82 +55,72 @@ window.onload = function() {
         generate_equations();
     }
 
-    function create()
-    {
+    function create() {
         cursor = 0;
         Graphics.drawBackground(game);
         
         game.input.gamepad.start();
 
         gamepad = game.input.gamepad.pad1;
-        // console.log(game.input.gamepad.pad1);
-        // console.log(game.input.gamepad.pad2);
-        // console.log(game.input.gamepad.pad3);
 
         Q = game.input.keyboard.addKey(Phaser.Keyboard.Q);
         Q.onDown.add(onQ, this);
+
         E = game.input.keyboard.addKey(Phaser.Keyboard.E);
         E.onDown.add(onE, this);
+
         R = game.input.keyboard.addKey(Phaser.Keyboard.R);
         R.onDown.add(onR, this);
+
         Space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         Space.onDown.add(onSpace, this);
     }
 
-    function onR()
-    {
+    function onR() {
         console.log("Question : "  + questions[question_index]);
         console.log("Current Answer: " + answers[cursor]);
     }
 
 
-    function onQ()
-    {
+    function onQ() {
         console.log("Rotating Cursor CCW - (Cursor, Val): (" + cursor + ", " + answers[cursor] + ")");
-        if(cursor - 1 < 0)
+        if (cursor - 1 < 0) {
             cursor = answers.length - 1;
-        else 
+        } else  {
             cursor = cursor - 1;
+        }
         console.log("Rotated Cursor CCW - NEW (Cursor, Val): (" + cursor + ", " + answers[cursor] + ")");
     }
 
-    function onE()
-    {
+    function onE() {
         console.log("Rotating Cursor CCW - (Cursor, Val): (" + cursor + ", " + answers[cursor] + ")");
         cursor = (cursor + 1) % answers.length;    
         console.log("Rotated Cursor CCW - NEW  (Cursor, Val): (" + cursor + ", " + answers[cursor] + ")");
     }
 
-    function onSpace()
-    {
+    function onSpace() {
         lock_in_answer();
     }
 
-    function update()
-    {
-        if(question_index  === questions.length){
+    function update() {
+        if (question_index  === questions.length) {
             alert('You Win!');
             preload();       
             return;
         }
         gamepad_enabled = game.input.gamepad.supported && game.input.gamepad.active && gamepad.connected;
-        // console.log(gamepad_enabled);
-        if(gamepad_enabled){
+        if (gamepad_enabled) {
             process_gamepad_controls();
         }
     }
 
-    function process_gamepad_controls()
-    {
+    function process_gamepad_controls() {
         process_left_joystick();
-        //process_right_joystick();
         process_gamepad_buttons();
     }
 
-    function process_gamepad_buttons()
-    {
-
-        if(gamepad.justPressed(Phaser.Gamepad.XBOX360_A) && cursor != -1){
+    function process_gamepad_buttons() {
+        if (gamepad.justPressed(Phaser.Gamepad.XBOX360_A) && cursor != -1) {
             lock_in_answer();
         }
     }
@@ -134,57 +128,41 @@ window.onload = function() {
     function lock_in_answer() {
         console.log('question: ' + questions[question_index]);
         console.log('locked in answer: ' + answers[cursor]);
-        if(eval(questions[question_index]) === answers[cursor] && !(cursor in answered_questions))
-        {
+        if (eval(questions[question_index]) === answers[cursor] && !(cursor in answered_questions)) {
             answered_questions[cursor] = true;
             //cannot use this cursor index for answer again.
             question_index += 1
             console.log("Correct!");;
-        }
-        else if(cursor in answered_questions)
-        {
+        } else if(cursor in answered_questions) {
             console.log("answer: " + answers[cursor] + " @ cursor: " + cursor + " already used");
         }
     }
 
-    function process_left_joystick()
-    {
+    function process_left_joystick() {
         cursor = -1;
         var controller_moved_x = Math.abs(gamepad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X)) > leftjs_dead_zone;
         var controller_moved_y = Math.abs(gamepad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y)) > leftjs_dead_zone;
-        if(controller_moved_x || controller_moved_y)
-        {
+        if (controller_moved_x || controller_moved_y) {
             var x = gamepad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X);
             var y = -gamepad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y);
             var tmp  = Math.atan2(y, x);
-            if(y < 0)
-            {
+            if (y < 0) {
                 tmp += 2 * Math.PI;
                 cursor = tmp;
                 cursor *= 360 / (2 * Math.PI)
-
-            }
-            else
-            {
+            } else {
                 cursor = tmp;
                 cursor *= 360 / (2 * Math.PI)
-
             }
             cursor = Math.abs(cursor);
             map_cursor_to_wheel(cursor);
         }
-        // if(cursor != -1)
-            // console.log(''+cursor + ' -> ' + wheel_map[''+difficulty][cursor]);
-        // else
-            // console.log('neutral');
     }
 
-    function map_cursor_to_wheel(cursor_val)
-    {
+    function map_cursor_to_wheel(cursor_val) {
         var wheel = wheel_map[''+difficulty]
-        for(var i = 0; i < wheel.length; i ++)
-        {
-            if(cursor_val <= wheel[i]){
+        for (var i = 0; i < wheel.length; i++) {
+            if (cursor_val <= wheel[i]) {
                 cursor = i;
                 return;
             }
@@ -193,54 +171,53 @@ window.onload = function() {
         return;
     }
 
-    function process_right_joystick()
-    {
+    function process_right_joystick() {
 
     }
 
-    function init_input()
-    {
+    function init_input() {
 
     }
    
-    function generate_equations()
-    {
+    function generate_equations() {
         let length = wheel_map[''+difficulty].length;
         let nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
         questions = [];
         answers = [];
         let fractions = false;
         let ops = ['+', '-'];
-        if(grade >= 3) {
+        if (grade >= 3) {
             ops.push('*');
             ops.push('/');
         }
-        if(grade % 2 == 0)
+        if (grade % 2 == 0) {
             fractions = true;
+        }
         
         let j = 0;
-        while(j < length)
-        {
+        while (j < length) {
             let str = '';
             let numerator_1 = nums[game.rnd.integerInRange(0, nums.length - 1)];
             str += numerator_1 + ' ';
-            if(fractions)
+            if (fractions) {
                 str += + '/' + nums[game.rnd.integerInRange(0, nums.length - 1)] + ' ';
+            }
             let op = ops[game.rnd.integerInRange(0, ops.length - 1)];
             str += op + ' ';
             let lower_bound = nums.length - 1;
-            if(op === '-')
+            if (op === '-') {
                 lower_bound = numerator_1;
+            }
             let numerator_2 = nums[game.rnd.integerInRange(0, lower_bound)];
             str += numerator_2 + ' ';
-            if(fractions)
+            if (fractions) {
                 str += '/' + nums[game.rnd.integerInRange(0, nums.length - 1)];
+            }
             let result = eval(str);
             
-            if(questions.indexOf(str) !== -1)
+            if (questions.indexOf(str) !== -1) {
                 continue;
-            else
-            {
+            } else {
                 j++;
                 questions.push(str);
                 answers.push(result);
